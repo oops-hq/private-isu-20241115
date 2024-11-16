6a3fce038ac2bd311e42e3fbca7800e2a47035b4
KOJIMA Kazunori <kjm.kznr@gmail.com>
2024-11-16 18:09:21 +0900

    users.del_flag にインデックスを追加
    {"pass":true,"score":32089,"success":31815,"fail":0,"messages":[]}

--- webapp/sql/zz_index.sql
+++ webapp/sql/zz_index.sql
@@ -4,3 +4,5 @@ alter table comments add index idx_comments_post_id_created_at (post_id, created
 alter table comments add index idx_comments_user_id (user_id);
 alter table posts add index idx_posts_created_at (created_at);
 alter table posts add index idx_posts_user_id_created_at (user_id, created_at);
+
+alter table users add index idx_del_flag (del_flg);
